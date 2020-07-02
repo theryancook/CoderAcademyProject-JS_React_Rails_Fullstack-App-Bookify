48ca37ef47c61a6a07f20f20485c211c71ef5df2
theryancook <ryancook12@gmail.com>
2020-07-02 15:52:02 +1000

    rendered books

--- bookify-frontend/src/ViewBooks.js
+++ bookify-frontend/src/ViewBooks.js
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ViewBooks = () => {
 
+    const [books, setBooks] = useState([])
+
     useEffect(() => {
+        axios.get(`http://localhost:3000/books`)
+        .then(res => {
+            setBooks(res.data)
+        })
 
     }, [])
 
     return (
         <div>
-            View Books
+            {books.map((book, index) => (
+                <li key={index}>
+                    {book.title} by {book.author}.
+                    <br/> 
+                    The genre is {book.genre}.
+                    <br/><br/> 
+                </li>
+            ))}
         </div>
     )
 }
