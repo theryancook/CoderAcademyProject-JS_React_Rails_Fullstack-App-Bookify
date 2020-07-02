d47566284727dfb71579763ba4117f737c616c4c
theryancook <ryancook12@gmail.com>
2020-07-02 16:02:34 +1000

    implemented error handling

--- bookify-frontend/src/ViewBooks.js
+++ bookify-frontend/src/ViewBooks.js
@@ -4,17 +4,23 @@ import axios from 'axios'
 const ViewBooks = () => {
 
     const [books, setBooks] = useState([])
+    const [errorMessage, setErrorMessage] = useState("error")
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/books`)
+        axios.get(`http://localhost:3000/book`)
         .then(res => {
             setBooks(res.data)
+        })
+        .catch(e => {
+            setErrorMessage(e.message)
+            console.log(e)
         })
 
     }, [])
 
     return (
         <div>
+            {errorMessage && <h3>{errorMessage}</h3>}
             {books.map((book, index) => (
                 <li key={index}>
                     {book.title} by {book.author}.
